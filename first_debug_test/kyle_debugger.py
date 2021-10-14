1f075c74b28b34b186fae17941d10b6384cb7004
Huberdoggy <huberdoggy@gmail.com>
2021-10-14 05:55:26 -0500

    added defs for x64 context in structs as specified on StackOverflow. Re-worked thread_enum to use these and print Rxx registers vs Exx

--- first_debug_test/kyle_debugger.py
+++ first_debug_test/kyle_debugger.py
@@ -1,36 +1,41 @@
 from ctypes import *
 from kyle_debugger_defs import *
 
+
 kernel32 = windll.kernel32
 
 
 class Debugger():
-    def __init__(self):  # Init w/ flags off
+
+    def __init__(self):
         self.h_process = None
         self.pid = None
         self.debugger_active = False
+        # self.h_thread = None
+        # self.context = None
 
     def load(self, path_to_exe):
-        # dwCreation flag determines howto create the proc
-        # set creation_flags = CREATE_NEW_CONSOLE to see the calc GUI
-        creation_flags = DEBUG_PROCESS  # from our type defs
-        # creation_flags = CREATE_NEW_CONSOLE
 
-        # instantiate the structs...
-        startup_information = STARTUPINFO()
+        # dwCreation flag determines how to create the process
+        # set creation_flags = CREATE_NEW_CONSOLE if you want
+        # to see the calculator GUI
+        creation_flags = DEBUG_PROCESS
+
+        # instantiate the structs
+        startupinfo = STARTUPINFO()
         process_information = PROCESS_INFORMATION()
 
-        # These opts allow the new process spawn separate window.
-        # This helps us identify how various settings in StartupInfo struct
-        # can affect the debugger
-        startup_information.dwFlags = 0x1  # True
-        startup_information.wShowWindow = 0x0  # False
+        # The following two options allow the started process
+        # to be shown as a separate window. This also illustrates
+        # how different settings in the STARTUPINFO struct can affect
+        # the debugger.
+        startupinfo.dwFlags = 0x1
+        startupinfo.wShowWindow = 0x0
 
-        # Here, we init the cb var in StartupInfo struct
-        # a.k.a => the size of the struct itself
-        startup_information.cb = sizeof(startup_information)
+        # We then initialize the cb variable in the STARTUPINFO struct
+        # which is just the size of the struct itself
+        startupinfo.cb = sizeof(startupinfo)
 
-        # set all params to Null except the those essential to create debug proc
         if kernel32.CreateProcessA(path_to_exe,
                                    None,
                                    None,
@@ -39,34 +44,39 @@ class Debugger():
                                    creation_flags,
                                    None,
                                    None,
-                                   byref(startup_information),
+                                   byref(startupinfo),
                                    byref(process_information)):
+
             print "[*] We have successfully launched the process!"
+            print "[*] The PID is: %d" % process_information.dwProcessId
+            self.pid = process_information.dwProcessId
+            # Obtain a handle to the newly created process and store it for future use
+            self.h_process = self.open_process(process_information.dwProcessId)
+            self.debugger_active = True
         else:
-            print "[*] We have successfully launched the process!"
-            print "[*] PID: {} ".format(process_information.dwProcessId)
-        # Obtain a valid handle to the new created proc and store it
-        self.h_process = self.open_process(process_information.dwProcessId)
+            print "[*] Error with error code %d." % kernel32.GetLastError()
 
     def open_process(self, pid):
-        # Obtain correct access rights to perform debugging
-        # Param for bInheritHandle will always be set False
-        h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS, pid, False)
+
+        h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, pid)
         return h_process
 
     def attach(self, pid):
+
         self.h_process = self.open_process(pid)
-        # Attempt to attach to the proc, if fail, exit func call..
+
+        # We attempt to attach to the process
+        # if this fails we exit the call
         if kernel32.DebugActiveProcess(pid):
             self.debugger_active = True
             self.pid = int(pid)
-            self.run()
         else:
-            print "[!] Unable to attach to the process."
+            print "[*] Unable to attach to the process."
 
     def run(self):
-        # Poll the debugger for debugging events
 
+        # Now we have to poll the debugger for
+        # debugging events           
         while self.debugger_active:
             self.get_debug_event()
 
@@ -76,20 +86,64 @@ class Debugger():
         continue_status = DBG_CONTINUE
 
         if kernel32.WaitForDebugEvent(byref(debug_event), INFINITE):
-            # We aren't going to build any event handlers yet, just resume the proc
-            # raw_input("Press any key to continue... => ")
-            # self.debugger_active = False  # flip flag off
-            kernel32.ContinueDebugEvent(
-                debug_event.dwProcessId,
-                debug_event.dwThreadId,
-                continue_status
-            )
+            # We aren't going to build any event handlers yet
+            # Just resume the proc for now.
+            # raw_input("Press any key to continue => ")
+            # self.debugger_active = False
+            kernel32.ContinueDebugEvent(debug_event.dwProcessId,
+                                        debug_event.dwThreadId,
+                                        continue_status)
 
     def detach(self):
 
-        if kernel32.DebugActiveProcessStop(self.pid):  # if successfully identified end
-            print "[+] Finished debugging. Exiting.."
+        if kernel32.DebugActiveProcessStop(self.pid):
+            print "[*] Finished debugging. Exiting..."
             return True
         else:
-            print "[!] An error occurred."
+            print "[!] There was an error"
+            return False
+
+    def open_thread(self, thread_id):
+
+        h_thread = kernel32.OpenThread(THREAD_ALL_ACCESS, None, thread_id)
+
+        if h_thread is not None:
+            return h_thread
+        else:
+            print "[*] Could not obtain a valid thread handle."
+            return False
+
+    def enumerate_threads(self):
+
+        thread_entry = THREADENTRY32()
+        thread_list = []
+        snapshot = kernel32.CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, self.pid)
+
+        if snapshot is not None:
+
+            # You have to set the size of the struct
+            # or the call will fail
+            thread_entry.dwSize = sizeof(thread_entry)
+            success = kernel32.Thread32First(snapshot, byref(thread_entry))
+            while success:
+                if thread_entry.th32OwnerProcessID == self.pid:
+                    thread_list.append(thread_entry.th32ThreadID)
+                success = kernel32.Thread32Next(snapshot, byref(thread_entry))
+            # No need to explain this call, it closes handles
+            # so that we don't leak them.
+            kernel32.CloseHandle(snapshot)
+            return thread_list
+        else:
+            return False
+
+    def get_thread_context(self, thread_id):
+        # Modified for platform compatibility - added class structs in defs
+        context = CONTEXT64()
+        context.ContextFlags = CONTEXT_FULL | CONTEXT_DEBUG_REGISTERS
+        # Obtain a handle to the thread
+        h_thread = self.open_thread(thread_id)
+        if kernel32.GetThreadContext(h_thread, byref(context)):
+            kernel32.CloseHandle(h_thread)
+            return context
+        else:
             return False
--- first_debug_test/run_test.py
+++ first_debug_test/run_test.py
@@ -1,8 +1,22 @@
-import kyle_debugger  # import the logic and start a process
+import kyle_debugger
+from kyle_debugger_defs import *
 
 debugger = kyle_debugger.Debugger()
-#debugger.load("C:\\Windows\\System32\\calc.exe")
-pid = raw_input("Enter the PID of the process to attach to => ")
-debugger.attach(int(pid)) # of course we need to convert raw_inp str
-debugger.detach() # then call the exit func
-
+pid = raw_input("Enter the PID of the process to attach to: ")
+debugger.attach(int(pid))
+lst = debugger.enumerate_threads()
+# For each thread in the list we want to
+# grab the value of each of the registers
+for thread in lst:
+    thread_context = debugger.get_thread_context(thread)
+    # Now let's output the contents of some of the registers
+    print "[*] Dumping registers for thread ID: 0x%08x" % thread
+    print "[**] RIP: 0x%08x" % thread_context.Rip
+    print "[**] RSP: 0x%08x" % thread_context.Rsp
+    print "[**] RBP: 0x%08x" % thread_context.Rbp
+    print "[**] RAX: 0x%08x" % thread_context.Rax
+    print "[**] RBX: 0x%08x" % thread_context.Rbx
+    print "[**] RCX: 0x%08x" % thread_context.Rcx
+    print "[**] RDX: 0x%08x" % thread_context.Rdx
+    print "[*] END DUMP"
+debugger.detach()
